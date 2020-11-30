d7902a304658cf53175a866714426666b40b62df
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-11-30 13:10:48 +0500

    dsfds

--- components/root.js
+++ components/root.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 
 const styles = StyleSheet.create({
@@ -7,8 +7,15 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
     },
+    text : {
+        marginTop : 40,
+        fontWeight : 'bold'
+    },
+    view : {
+        marginTop : 20,
+        fontStyle : 'italic'
+    }
 });
 
 // class Root extends Component {
@@ -32,10 +39,17 @@ const styles = StyleSheet.create({
 import { useQuery, gql } from "@apollo/client";
 
 const MY_DATA = gql`
-{
-    user(id: 1) {
-      id
-      name
+query (
+    $options: PageQueryOptions
+  ) {
+    posts(options: $options) {
+      data {
+        id
+        title
+      }
+      meta {
+        totalCount
+      }
     }
   }
 `
@@ -44,13 +58,20 @@ const Root = () => {
 
     const { loading, error, data } = useQuery(MY_DATA)
 
-    if(loading) return <Text>Loading ...</Text>
-    if(error) return <Text>Error !!</Text>
+    if (loading) return <Text>Loading ...</Text>
+    if (error) return <Text>Error !!</Text>
 
-    console.log(data)
+    // console.log(data.posts.data[0].title)
+    const mydata = data.posts.data
+    console.log(mydata)
     return (
         <View style={styles.container} >
-            <Text> Welcome Root </Text>
+            <Text style={styles.text} > Welcome Root </Text>
+                <FlatList
+                    data={mydata}
+                    renderItem={(item) => (<Text>{ item.item.id } </Text>)}
+                    keyExtractor={(item) => item.id.toString()}
+                />
         </View>
     )
 }
